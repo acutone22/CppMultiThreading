025308378612fc396c526d1aea60d3a6124da1ec
alberto <alberto.cutone@gmail.com>
2022-05-09 16:17:56 +0200

    Thread joinability

--- MultiThread/MultiThread/MultiThread.cpp
+++ MultiThread/MultiThread/MultiThread.cpp
@@ -23,7 +23,9 @@ public:
 int main()
 {
     std::thread thread1(foo);
+    cout << "Thread1 is" << (thread1.joinable() ? "" : " not") << " joinable" << endl;
     thread1.join();
+    cout << "After call join(), thread1 is" << (thread1.joinable() ? "" : " not") << " joinable" << endl;
 
     callable_class obj;
     std::thread thread2(obj);
@@ -37,14 +39,3 @@ int main()
 
     cout << "Running main function" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# CppMultiThreading
-Join vs Detach
+### CppMultiThreading
+#Thread Join vs Detach
 - join force the main thread to wait until the thread that is called upon finishes its execution
 - detach let multiple threads run in parallel
+  
+If you do not join or detach a joinable thread, at thread destructor time, std::terminate will be called (unsafe program).
+A default constructed (no argument) thread object is not joinable by definition.
